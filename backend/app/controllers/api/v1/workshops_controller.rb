51f12417f1d927e07c409054ef146988b995bdbb
Abigail Galzin <abigail.galzin@assuresoft.com>
2026-07-30 16:47:36 -0400

    feat: enhance WorkshopsController and Workshop model validation

--- backend/app/controllers/api/v1/workshops_controller.rb
+++ backend/app/controllers/api/v1/workshops_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::WorkshopsController < ApplicationController
   def index
     workshops = Workshop.includes(:sessions).where(active: true)
 
-    pagy, records = pagy(workshops, limit: 10)
+    pagy, records = pagy(workshops)
     response = Response::ResponseData.new(
-      data: workshops.as_json(
+      data: records.as_json(
         except: [:created_at, :updated_at],
         include: { sessions: { only: [:starts_at, :ends_at, :capacity, :status ]}}
       ),
@@ -19,11 +19,11 @@ class Api::V1::WorkshopsController < ApplicationController
 
   # POST /api/v1/workshops
   def create
-    @workshop = Workshop.new(workshop_params)
+    workshop = Workshop.new(workshop_params)
 
-    if @workshop.save
+    if workshop.save
       response = Response::ResponseData.new(
-        data: @workshop.as_json(
+        data: workshop.as_json(
           include: { sessions: { only: [:starts_at, :ends_at, :capacity, :status ]}}
         ),
         message: I18n.t('success.creation', model: Workshop.model_name.human),
@@ -32,7 +32,14 @@ class Api::V1::WorkshopsController < ApplicationController
 
       render json: response.as_json, status: response.status
     else
-      render json: { errors: @workshop.errors.full_messages }, status: :unprocessable_entity
+      response = Response::ResponseError.new(
+        code: "creation_conflict",
+        message: I18n.t('errors.create_error', model: Workshop.model_name.human),
+        details: workshop.errors.full_messages,
+        status: :unprocessable_entity
+      )
+
+      return render json: response.as_json, status: response.status
     end
   end
 
