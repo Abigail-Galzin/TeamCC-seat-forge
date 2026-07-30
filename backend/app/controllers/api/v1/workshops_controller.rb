7eb860a4eb5fb3318ccf7cc15e80af354dfa0a8d
Abigail Galzin <abigail.galzin@assuresoft.com>
2026-07-30 13:11:01 -0400

    feat: add index action to WorkshopsController and enhance creation response format

--- backend/app/controllers/api/v1/workshops_controller.rb
+++ backend/app/controllers/api/v1/workshops_controller.rb
@@ -1,14 +1,38 @@
 class Api::V1::WorkshopsController < ApplicationController
 
+  def index
+    workshops = Workshop.includes(:sessions).where(active: true)
+
+    pagy, records = pagy(workshops, limit: 10)
+    response = Response::ResponseData.new(
+      data: workshops.as_json(
+        except: [:created_at, :updated_at],
+        include: { sessions: { only: [:starts_at, :ends_at, :capacity, :status ]}}
+      ),
+      message: I18n.t('success.response', model: Workshop.model_name.human.pluralize),
+      status: :ok
+    )
+
+    render json: response.as_json.merge(Response::ResponsePaginationInfo.new(pagy).as_json),
+      status: response.status
+  end
 
   # POST /api/v1/workshops
   def create
     @workshop = Workshop.new(workshop_params)
 
     if @workshop.save
-      render json: @workshop, status: :created
+      response = Response::ResponseData.new(
+        data: @workshop.as_json(
+          include: { sessions: { only: [:starts_at, :ends_at, :capacity, :status ]}}
+        ),
+        message: I18n.t('success.creation', model: Workshop.model_name.human),
+        status: :created
+      )
+
+      render json: response.as_json, status: response.status
     else
-      render json: @workshop.errors, status: :unprocessable_entity
+      render json: { errors: @workshop.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
